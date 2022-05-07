ae08b44d461771acc8292c979693dda93d503dc6
le-gonz <antoine.beaudoux@epitech.eu>
2022-05-08 01:01:23 +0200

    add: sign in button on banner

--- frontend/src/component/Banner.svelte
+++ frontend/src/component/Banner.svelte
@@ -2,4 +2,28 @@
     import SizedBox from "../lib/SizedBox.svelte";
 </script>
 
-<SizedBox backgroundColor="white" width="100%" height={70} margin="-10px" />
+<SizedBox backgroundColor="black" width="100%" height={100}>
+    <div class="button-place">
+        <button class="button-sign">Sign In</button>
+    </div>
+</SizedBox>
+
+<style>
+    .button-place {
+        display: flex;
+        align-items: center;
+        justify-content: end;
+    }
+    .button-sign {
+        background-color:white;
+        width: 150px;
+        height: 100px;
+        border: 0px;
+        color: black;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    .button-sign:hover {
+        opacity: 0.8;
+    }
+</style>
